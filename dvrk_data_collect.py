448ed432b5ef6c8411ee50d90bd51d22ee1e95c3
jtzhangLogan <jzhan247@jhu.edu>
2021-10-28 19:06:12 -0400

    updated script

--- dvrk_data_collect.py
+++ dvrk_data_collect.py
@@ -1,6 +1,6 @@
 # derived from dvrk_bag_replay
 
-import dvrk
+import crtk
 import os
 import sys
 import csv
@@ -14,6 +14,29 @@ import numpy
 import PyKDL
 import argparse
 import subprocess
+
+
+# simplified arm class to replay motion
+class arm_custom:
+    
+    # simplified jaw class to close gripper
+    class __Jaw:
+        def __init__(self, ros_namespace, expected_interval, operating_state_instance):
+            self.__crtk_utils = crtk.utils(self, ros_namespace, expected_interval, operating_state_instance)
+            self.__crtk_utils.add_move_jp()
+
+    def __init__(self, device_namespace, expected_interval):
+        # ROS initialization
+        if not rospy.get_node_uri():
+            rospy.init_node('simplified_arm_class', anonymous = True, log_level = rospy.WARN)
+        # populate this class with all the ROS topics we need
+        self.crtk_utils = crtk.utils(self, device_namespace, expected_interval)
+        self.crtk_utils.add_operating_state()
+        self.crtk_utils.add_servo_jp()
+	self.crtk_utils.add_move_jp()
+
+        self.jaw = self.__Jaw(device_namespace + '/jaw', expected_interval, operating_state_instance = self)
+
 
 
 # helper function to kill the rosbag process
@@ -44,14 +67,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--arm', type=str, required=True,
                     choices=['ECM', 'MTML', 'MTMR', 'PSM1', 'PSM2', 'PSM3'],
                     help='arm name corresponding to ROS topics without namespace.')
-parser.add_argument('-c', '--csv', type=argparse.FileType('r'), required=True,
+parser.add_argument('-f', '--csv', type=argparse.FileType('r'), required=True,
                     help='csv file containing the trajectory to replay.')
 parser.add_argument('-s', '--setup', type=str, required=True,
                     choices=['PSM', 'PSM-SUJ'],
                     help='description of your dvrk setup, necessary for collect shell script selection.')
-parser.add_argument('-i', '--id', type=str, required=True,
-                    choices=['1', '2', '3'],
-                    help='your PSM ID, e.g., if you are using PSM2, then you should type -i 2')
+parser.add_argument('-c', '--config', type=str, required=True,
+                    help='index of your current configuration, if you changed SUJ position, please use another index. e.g., \"-c 1\" for configuration 1 and \"-c 2\" for for configuration 1')
+
 
 args = parser.parse_args(argv[1:])  # skip argv[0], script name
 
@@ -62,16 +85,24 @@ args = parser.parse_args(argv[1:])  # skip argv[0], script name
 if not os.path.exists(os.path.join(os.getcwd(), 'data')):
     os.mkdir(os.path.join(os.getcwd(), 'data'))
 
+# create configuration sub_folder
 folder_name = os.path.join(os.getcwd(), 'data')
+sub_folder_name = os.path.join(os.getcwd(), os.path.join('data', args.config))
+if not os.path.exists(sub_folder_name):
+    os.mkdir(sub_folder_name)
 
 # determine script based on setup
+PSM_id = args.arm.strip('PSM')
 if args.setup == 'PSM':
-    command = "rosbag record -O {0}.bag /{1}/measured_cp /{1}/measured_js /{1}/jaw/measured_js /{1}/spatial/jacobian".format(os.path.join(folder_name, args.csv.name.split('.csv')[0]), 'PSM'+args.id)
+    command = "rosbag record -O {0}_{1}.bag /{1}/measured_cp /{1}/measured_js /{1}/jaw/measured_js /{1}/spatial/jacobian".format(os.path.join(sub_folder_name, args.csv.name.split('.csv')[0]), 'PSM'+str(PSM_id))
+    # sanity check command
+    sanity_cmd = "rosbag info {0}_{1}.bag".format(os.path.join(sub_folder_name, args.csv.name.split('.csv')[0]), 'PSM'+str(PSM_id))
+elif args.setup == 'PSM-SUJ':
+    command = "rosbag record -O {0}_{1}_SUJ.bag /{1}/measured_cp /{1}/measured_js /{1}/jaw/measured_js /{1}/spatial/jacobian /SUJ/{1}/measured_cp /SUJ/{1}/measured_js".format(os.path.join(sub_folder_name, args.csv.name.split('.csv')[0]), 'PSM'+str(PSM_id))
+    # sanity check command
+    sanity_cmd = "rosbag info {0}_{1}_SUJ.bag".format(os.path.join(sub_folder_name, args.csv.name.split('.csv')[0]), 'PSM'+str(PSM_id))
 else:
-    command = "rosbag record -O {0}.bag /{1}/measured_cp /{1}/measured_js /{1}/jaw/measured_js /{1}/spatial/jacobian /SUJ/{1}/measured_cp /SUJ/{1}/measured_js".format(os.path.join(folder_name, args.csv.name.split('.csv')[0]), 'PSM'+args.id)
-
-# sanity check command
-sanity_cmd = "rosbag info {0}.bag".format(os.path.join(folder_name, args.csv.name.split('.csv')[0]))
+    sys.exit('-- Unsupported setup')
 
 # ---------------------------------------------
 # read commanded joint position
@@ -80,6 +111,7 @@ poses = []
 with open(args.csv.name) as csvfile:
     for pose in csv.reader(csvfile, delimiter=','):
         poses.append(np.array(pose).astype(float))
+    csvfile.close()
 
 # ---------------------------------------------
 # prepare psm
@@ -87,7 +119,7 @@ with open(args.csv.name) as csvfile:
 print('-- This script will replay a trajectory defined in %s on arm %s' % (args.arm, args.csv.name))
 
 # create arm
-arm = dvrk.psm(arm_name=args.arm, expected_interval=0.001)
+arm = arm_custom(device_namespace=args.arm, expected_interval=0.001)
 
 # make sure the arm is powered
 print('-- Enabling arm')
@@ -102,7 +134,7 @@ input('---> Make sure the arm is ready to move using cartesian positions.  For a
 
 # close gripper
 input('---> Press \"Enter\" to move to close gripper')
-jaw_jp = np.array([0])
+jaw_jp = np.array([-10.0 * np.pi / 180.0])
 arm.jaw.move_jp(jaw_jp).wait()
 
 # go to initial position and wait
@@ -126,10 +158,17 @@ total = len(poses)
 start_time = time.time()
 
 for pose in poses:
+    start_t = time.time()
     arm.servo_jp(pose)
     counter = counter + 1
+    # report progress
     sys.stdout.write('\r-- Progress %02.1f%%' % (float(counter) / float(total) * 100.0))
     sys.stdout.flush()
+    end_t = time.time()
+    delta_t = 0.0018 - (end_t - start_t)
+    # if process takes time larger than console rate, don't sleep
+    if delta_t > 0:
+        time.sleep(delta_t)
 
 # stop bagging 
 terminate_process_and_children(rosbag_process)
@@ -138,4 +177,4 @@ print('\n--> Time to replay trajectory: %f seconds' % (time.time() - start_time)
 print('--> Done!')
 
 # check if required topics are collected correctly
-subprocess.Popen(sanity_cmd.split(' '))
+os.system(sanity_cmd)
